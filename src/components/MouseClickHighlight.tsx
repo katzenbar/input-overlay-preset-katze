23abc04531e01f786b02c0810ac13cbc6572b430
Kat Fairbanks <ktornwall@gmail.com>
2022-09-13 21:58:56 -0400

    Add click indicators for each mouse button

--- src/components/MouseClickHighlight.tsx
+++ src/components/MouseClickHighlight.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, MotionConfig } from "framer-motion";
 import { useMouseDownState } from "../hooks/useMouseDownState";
 import { useMousePosition } from "../hooks/useMousePosition";
 
-const size = 30;
+const mouseHighlightRadius = 15;
+const indicatorRatio = 0.4;
+
+const transition = { type: "spring", duration: 0.15, bounce: 0.3 };
 
 export const MouseClickHighlight: React.FC = () => {
   const mouseDownState = useMouseDownState();
   const mousePosition = useMousePosition();
+
+  const anyMouseButtonDown = mouseDownState[1] || mouseDownState[2] || mouseDownState[3];
+
+  const indicatorRadius = indicatorRatio * mouseHighlightRadius;
 
   return (
     <motion.div
@@ -15,27 +22,96 @@ export const MouseClickHighlight: React.FC = () => {
         position: "fixed",
         top: 0,
         left: 0,
-        transform: `translate(${mousePosition.x - 0.5 * size}px, ${mousePosition.y - 0.5 * size}px)`,
+        transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
       }}
     >
-      <AnimatePresence>
-        {mouseDownState[0] && (
-          <motion.div
-            key="demo"
-            className="border-4 border-red-500"
-            style={{
-              height: size,
-              width: size,
-              borderRadius: "100%",
-              transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
-            }}
-            transition={{ type: "spring", duration: 0.15, bounce: 0.3 }}
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0 }}
-          ></motion.div>
-        )}
-      </AnimatePresence>
+      <MotionConfig transition={transition}>
+        <AnimatePresence>
+          {mouseDownState[1] && (
+            <motion.div
+              key="lmb-highlight"
+              className="rounded-full	bg-red-500"
+              style={{
+                position: "absolute",
+                height: 2 * indicatorRadius,
+                width: 2 * indicatorRadius,
+                top: -indicatorRadius,
+                right: mouseHighlightRadius + 1.4 * indicatorRadius,
+              }}
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+            />
+          )}
+
+          {mouseDownState[2] && (
+            <>
+              <motion.div
+                key="rmb-highlight-1"
+                className="rounded-full	bg-red-500"
+                style={{
+                  position: "absolute",
+                  height: 2 * indicatorRadius,
+                  width: 2 * indicatorRadius,
+                  top: -2.4 * indicatorRadius,
+                  left: mouseHighlightRadius + 1.4 * indicatorRadius,
+                }}
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0 }}
+              />
+              <motion.div
+                key="rmb-highlight-2"
+                className="rounded-full	bg-red-500"
+                style={{
+                  position: "absolute",
+                  height: 2 * indicatorRadius,
+                  width: 2 * indicatorRadius,
+                  top: 0.4 * indicatorRadius,
+                  left: mouseHighlightRadius + 1.4 * indicatorRadius,
+                }}
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0 }}
+              />
+            </>
+          )}
+
+          {mouseDownState[3] && (
+            <motion.div
+              key="mmb-highlight"
+              className="rounded-full	bg-red-500"
+              style={{
+                position: "absolute",
+                height: 3 * indicatorRadius,
+                width: 2 * indicatorRadius,
+                bottom: mouseHighlightRadius + 1.4 * indicatorRadius,
+                right: -indicatorRadius,
+              }}
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+            />
+          )}
+
+          {anyMouseButtonDown && (
+            <motion.div
+              key="mouse-highlight"
+              className="rounded-full	border-4 border-red-500"
+              style={{
+                position: "absolute",
+                top: -mouseHighlightRadius,
+                left: -mouseHighlightRadius,
+                height: 2 * mouseHighlightRadius,
+                width: 2 * mouseHighlightRadius,
+              }}
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+            />
+          )}
+        </AnimatePresence>
+      </MotionConfig>
     </motion.div>
   );
 };
--- src/hooks/inputEvents/setupDocumentInputEvents.ts
+++ src/hooks/inputEvents/setupDocumentInputEvents.ts
@@ -1,18 +1,42 @@
 import { EmitInputEventFn } from "../useSubscribeToInputEvent";
 
+const convertMouseButtonToUiohook = (domButton: number): number => {
+  switch (domButton) {
+    case 0:
+      return 1;
+    case 1:
+      return 3;
+    case 2:
+      return 2;
+    case 3:
+      return 4;
+    case 4:
+      return 5;
+    default:
+      return 0;
+  }
+};
+
 export const setupDocumentInputEvents = (emitInputEvent: EmitInputEventFn): (() => void) => {
   const handleMouseEvent = (key: Parameters<EmitInputEventFn>[0]) => (ev: MouseEvent) => {
     emitInputEvent(key, {
       event_type: key,
       event_source: "web",
       time: Math.trunc(ev.timeStamp * 1000),
-      button: ev.button,
+      button: convertMouseButtonToUiohook(ev.button),
       clicks: 0,
       mask: 0,
       x: ev.clientX,
       y: ev.clientY,
     });
   };
+
+  const blockContextMenu = (ev: MouseEvent) => {
+    ev.preventDefault();
+    ev.stopPropagation();
+  };
+
+  document.addEventListener("contextmenu", blockContextMenu);
 
   const onMouseDown = handleMouseEvent("mouse_pressed");
   const onMouseUp = handleMouseEvent("mouse_released");
@@ -23,6 +47,7 @@ export const setupDocumentInputEvents = (emitInputEvent: EmitInputEventFn): (() 
   document.addEventListener("mousemove", onMouseMove);
 
   return () => {
+    document.removeEventListener("contextmenu", blockContextMenu);
     document.removeEventListener("mouseup", onMouseUp);
     document.removeEventListener("mousedown", onMouseDown);
     document.removeEventListener("mousemove", onMouseMove);
